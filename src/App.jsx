88df845e5ef90fbb67c8e031c11322e31e542276
Gustavo Fernando Balbino <gustavo.balbino@pca.com.br>
2021-04-09 08:47:32 -0300

    aula 4 - exibindo post e categoria

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import './assets/css/base/base.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React from 'react';
+import './assets/css/base/base.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Home from './paginas/Home'
-import Sobre from './paginas/Sobre'
-import Pagina404 from './paginas/Pagina404'
-import Cabecalho from './components/Cabecalho'
+import Home from './paginas/Home';
+import Sobre from './paginas/Sobre';
+import Pagina404 from './paginas/Pagina404';
+import Cabecalho from './components/Cabecalho';
+import Post from './paginas/Post';
 
 function App() {
 
@@ -19,6 +20,9 @@ function App() {
         <Route path='/sobre'>
           <Sobre />
         </Route>
+        <Route path='/posts/:id'>
+          <Post />
+        </Route>
         <Route>
           <Pagina404 />
         </Route>
@@ -26,5 +30,6 @@ function App() {
     </Router>
   )
 }
+
 
 export default App
